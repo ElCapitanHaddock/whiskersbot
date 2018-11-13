458117e948a2e2326258ec2ac9a0dadc39c0ae1c
sbojevets <jkid@littlefish.us>
2018-11-13 01:04:41 +0000

    testing propose

--- server.js
+++ server.js
@@ -7,11 +7,47 @@ client.on('ready', () => {
  
 client.on('message', msg => {
   if (msg.content.includes(client.user.toString())) {
-    console.log(msg.content.split(""))
-    msg.channel.send(msg.author.toString());
+    var inp = msg.content.split(" ")
+    var cmd = inp[1]
+    var ctx = inp[2]
+    if (helper.func[cmd] == null)
+        msg.channel.send(msg.author.toString() + ", the command '" + cmd + "' does not exist.")
+    else if (ctx == null)
+        msg.channel.send(msg.author.toString() + ", please provide command context.")
+    else {
+        helper.func[cmd](msg.author.toString(), ctx, function(error, res) {
+            if (error) msg.channel.send(error)
+            else {
+                msg.channel.send(res);
+            }
+        })
+        msg.channel.send(msg.author.toString());
+    }
   }
 });
 
+var Helper = function() {
+    var self = this;
+    self.voting_channel = client.channels.find("name","mod-voting");
+    
+    self.func = {};
+    self.func.propose = function(author, ctx, cb) {
+        if (self.voting_channel == null) {
+            cb("Please add a channel called 'mod-voting' in order to create a proposal", null)
+        }
+        else {
+            var prop_id = Math.random().toString(36).substring(5);
+            self.voting_channel.send(
+                "Proposal #" + prop_id + "\n" + 
+                "Author: " + author + "\n" +
+                "Info: " + ctx 
+                );
+            cb(null, "Proposal succesfully sent.")
+        }
+    }
+}
+
+var helper = new Helper();
 var bot_secret_token = "NTExNjcyNjkxMDI4MTMxODcy.DsuUfQ.knMgnXhf2FOTWau5wi6yB9n0tVo"
 
 client.login(bot_secret_token)
