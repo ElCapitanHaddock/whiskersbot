6e15f1a72c56f549f3412945b5b194f0bbdb1d17
ElCapitanHaddock <jkid@littlefish.us>
2018-11-13 17:46:04 +0000

    beautify

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ var Helper = function() {
                 "Author: " + msg.author.toString() + "\n" +
                 "```" + ctx.trim() + "```\n"
                 );
-            cb(null, msg.author.toString() + " here's your id '" + prop_id + "'")
+            cb(null, msg.author.toString() + " your ID: *" + prop_id + "*")
         }
     }
 }
