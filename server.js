f17769db005b8e8753f05dd99a090f935295efb1
ElCapitanHaddock <jkid@littlefish.us>
2018-11-13 16:41:08 +0000

    api key secured

--- server.js
+++ server.js
@@ -11,25 +11,34 @@ client.on('ready', () => {
         Success/fail message is sent to both #announcements
 */
 client.on('message', msg => {
-  if (msg.content.includes(client.user.toString()) && !msg.author.bot) {
-    var inp = msg.content.trim().substr(client.user.toString().length+1);
-    var cmd = inp.substr(0,inp.indexOf(' '))
-    var ctx = inp.substr(inp.indexOf(' '), inp.length)
-    if (inp.indexOf(' ') == -1) 
-        msg.channel.send("epic fail")
-    else if (cmd == null)
-        msg.channel.send("lol ping Uhtred for help noob")
-    else if (helper.func[cmd] == null)
-        msg.channel.send(msg.author.toString() + " the command '" + cmd + "' does not exist idiot")
-    else if (ctx == null)
-        msg.channel.send(msg.author.toString() + " give context you imbecile")
-    else {
-        helper.func[cmd](msg, ctx, function(error, res) {
-            if (error) msg.channel.send(error)
-            else {
-                msg.channel.send(res)
-            }
-        })
+  if (msg.content.includes(client.user.toString()) && !msg.author.bot) { //use msg.member.roles
+    var m = msg.member.roles.find(function(role) {
+        if (role == "modera" || role == "admib") return true
+        else return null
+    })
+    if (m) { //if moderator or admin
+        var inp = msg.content.trim().substr(client.user.toString().length+1);
+        var cmd = inp.substr(0,inp.indexOf(' '))
+        var ctx = inp.substr(inp.indexOf(' '), inp.length)
+        if (inp.indexOf(' ') == -1) 
+            msg.channel.send("epic fail")
+        else if (cmd == null)
+            msg.channel.send("lol ping Uhtred for help noob")
+        else if (helper.func[cmd] == null)
+            msg.channel.send(msg.author.toString() + " the command '" + cmd + "' does not exist idiot")
+        else if (ctx == null)
+            msg.channel.send(msg.author.toString() + " give context you imbecile")
+        else {
+            helper.func[cmd](msg, ctx, function(error, res) {
+                if (error) msg.channel.send(error)
+                else {
+                    msg.channel.send(res)
+                }
+            })
+        }
+    }
+    else { //not moderator or admin
+        msg.channel.send(msg.author.toString() + " noob")
     }
   }
 });
@@ -69,10 +78,29 @@ client.on('messageReactionAdd', reaction => {
             }
         }
     }
+    else if (reaction.message.channel.name == "suggestions") {
+        if (reaction._emoji.name == "updoge") {
+            var upvotes = reaction.count;
+            if (upvotes >= 6) {
+                var ch = reaction.message.guild.channels.find(function(channel) {
+                  if (channel.name == "epic-mod-voting") {
+                    return channel
+                  } else return null
+                });
+                reaction.message.react('✅');
+                if (ch !== null) {
+                    ch.send(".................\n𝐏𝐄𝐓𝐈𝐓𝐈𝐎𝐍 @here" + "\n" + 
+                    "Author: " + reaction.message.author.toString() + "\n" +
+                    "Description: \n```" + reaction.message.content + "```\n"
+                    );
+                    reaction.message.channel.send("Petition Sent to Council:\n" + reaction.message.content)
+                }
+            }
+        }
+    }
 })
 
-var bot_secret_token = "NTExNjcyNjkxMDI4MTMxODcy.DsuUfQ.knMgnXhf2FOTWau5wi6yB9n0tVo"
-client.login(bot_secret_token)
+client.login(process.env.BOT_TOKEN)
 
 var Helper = function() {
     var self = this;
@@ -90,7 +118,7 @@ var Helper = function() {
         else {
             var prop_id = Math.random().toString(36).substring(5);
             ch.send(
-                ".................\n𝐏𝐑𝐎𝐏𝐎𝐒𝐀𝐋 @everyone" + "\n" + 
+                ".................\n𝐏𝐑𝐎𝐏𝐎𝐒𝐀𝐋 @here" + "\n" + 
                 "Author: " + msg.author.toString() + "\n" +
                 "Description: \n```" + ctx.trim() + "```\n"
                 );
