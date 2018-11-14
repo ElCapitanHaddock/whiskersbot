929eadffa53191e2dcb219d00fa93805e3de6042
ElCapitanHaddock <jkid@littlefish.us>
2018-11-14 17:20:38 +0000

    debugging

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const client = new Discord.Client(
  
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(client.channels);
     console.log(getChannel(client.channels, "epic-mod-voting"))
     /*.fetchMessages({ limit: 100 })
     .then(messages => console.log(`Received ${messages.size} messages`))
