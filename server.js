e2f290d99333fadd00054a0a9d413b974dea5dbc
ElCapitanHaddock <jkid@littlefish.us>
2018-11-18 21:53:58 +0000

    mention bugfix

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ client.on('ready', async() => {
         await guild.channels.find("id", "498157555416039454").fetchMessages({limit: 70}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("Test")
+            chat.send("Ohtred cumming online")
             /*
             chat.send("Hello, this is Uhtred speaking. My dad just deleted my discord, so I'm talking through the Capt.")
             chat.send("The report feature is now functional in #general and #serious. 5 :report: emojis deletes the message and reports it to a hidden mod-chat.")
@@ -77,7 +77,7 @@ client.on('ready', async() => {
 
 client.on('message', msg => {
     console.log(msg.author.username + " [" + msg.channel.name + "]: " + msg.content)
-    if (msg.content.includes("<@!511672691028131872>") && !msg.author.bot) { //use msg.member.roles
+    if ((msg.content.includes("<@!511672691028131872>") || msg.content.includes("<@511672691028131872>")) && !msg.author.bot) { //use msg.member.roles
         var m = msg.member.roles.find('name', 'modera') || msg.member.roles.find('name', 'admib')
         if (m) { //if moderator or admin
             var inp = msg.content.trim().substr(client.user.toString().length+1);
