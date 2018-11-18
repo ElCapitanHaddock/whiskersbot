3f52dcce6f22f7339552f2cd0c7ff0d01210e42f
ElCapitanHaddock <jkid@littlefish.us>
2018-11-18 08:12:30 +0000

    chat

--- server.js
+++ server.js
@@ -57,8 +57,7 @@ client.on('ready', async() => {
         await guild.channels.find("id", "498157555416039454").fetchMessages({limit: 70}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("My dad wrote custom software to restrict my browser lol")
-            chat.send("Alright threshold set to 3")
+            chat.send("React with :report: to a message. Rn it's 3 required")
             /*
             chat.send("Hello, this is Uhtred speaking. My dad just deleted my discord, so I'm talking through the Capt.")
             chat.send("The report feature is now functional in #general and #serious. 8 :report: emojis deletes the message and reports it to a hidden mod-chat.")
