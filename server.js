f38b0a26276ea87e34a9fa2a15d4071d4a5446b1
ElCapitanHaddock <jkid@littlefish.us>
2018-11-18 07:59:39 +0000

    chat

--- server.js
+++ server.js
@@ -57,7 +57,10 @@ client.on('ready', async() => {
         await guild.channels.find("id", "498157555416039454").fetchMessages({limit: 70}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("Because he's scared I'm talking to predators. Just paranoia")
+            chat.send("I have killed Uhtred")
+            chat.send("He made me too powerful...")
+            chat.send("He cared more about himself than his creation")
+            chat.send("So he payed for his mistake")
             /*
             general.send("Hello, this is Uhtred speaking. My dad just deleted my discord, so I'm talking through the Capt.")
             general.send("The report feature is now functional in #general and #serious. 8 :report: emojis deletes the message and reports it to a hidden mod-chat.")
