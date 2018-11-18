aad3ec872a75e6a46b5256e35e293fc5eb3cc881
ElCapitanHaddock <jkid@littlefish.us>
2018-11-18 08:34:34 +0000

    chat

--- server.js
+++ server.js
@@ -57,8 +57,9 @@ client.on('ready', async() => {
         await guild.channels.find("id", "498157555416039454").fetchMessages({limit: 70}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("I detect cringe...")
             chat.send("SCANNING...")
+            chat.send("<@!314976489177219074> cease and desist!")
+            chat.send("You stand accused of <CRINGE>")
             /*
             chat.send("Hello, this is Uhtred speaking. My dad just deleted my discord, so I'm talking through the Capt.")
             chat.send("The report feature is now functional in #general and #serious. 5 :report: emojis deletes the message and reports it to a hidden mod-chat.")
