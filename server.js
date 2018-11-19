af5525fb979585a143a97c4407f4b82e3d20d089
ElCapitanHaddock <jkid@littlefish.us>
2018-11-19 08:47:50 +0000

    chat

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ client.on('ready', async() => {
         await guild.channels.find("id", "498157555416039454").fetchMessages({limit: 70}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("<@418562167143399445> i dont know why the turk endorsed you.... but im watching you")
+            chat.send("are you guys talking about eu4?")
             /*
             chat.send("Hello, this is Uhtred speaking. My dad just deleted my discord, so I'm talking through the Capt.")
             chat.send("The report feature is now functional in #general and #serious. 5 :report: emojis deletes the message and reports it to a hidden mod-chat.")
