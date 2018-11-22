2b915c8331286a01c8bac02f3cdf45a83b6744c6
ElCapitanHaddock <jkid@littlefish.us>
2018-11-22 08:58:56 +0000

    chat

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ client.on('ready', async() => {
         await guild.channels.find("id", config.suggestions).fetchMessages({limit: config.fetch}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("can someone update me on okbr")
+            chat.send("4chan more like 4 chins you fat fucking reta")
         }
     }
 });
