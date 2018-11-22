3fee753343bb68d132461033aad3404f6ce66a08
ElCapitanHaddock <jkid@littlefish.us>
2018-11-22 09:03:43 +0000

    chat

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ client.on('ready', async() => {
         await guild.channels.find("id", config.suggestions).fetchMessages({limit: config.fetch}) //suggestion channel
         var chat = getChannel(guild.channels, "general")
         if (chat) {
-            chat.send("can someone describe that image for me")
+            //chat.send("can someone describe that image for me")
         }
     }
 });
