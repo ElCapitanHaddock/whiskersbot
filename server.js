c566445c9eed4da9184e65d9f0fba40b962a79a6
ElCapitanHaddock <jkid@littlefish.us>
2018-11-24 09:34:19 +0000

    testign express

--- server.js
+++ server.js
@@ -505,8 +505,6 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
-
-console.log(process.env)
