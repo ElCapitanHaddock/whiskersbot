8132816b06e1a6d0619277a561b2c4e29e0fc405
ElCapitanHaddock <jkid@littlefish.us>
2018-11-24 09:06:27 +0000

    testing web

--- server.js
+++ server.js
@@ -155,7 +155,6 @@ var configs =
     }
 ]
 
-console.log(process.env.HOST)
 
 const Discord = require('discord.js');
 const client = new Discord.Client(
@@ -497,3 +496,15 @@ var Helper = function() {
 }
 
 var helper = new Helper();
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
