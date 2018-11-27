72ec9f37fb5bd3bbc6a75799485dbe8acc256a25
ElCapitanHaddock <jkid@littlefish.us>
2018-11-27 17:45:22 +0000

    testing cleaned up code

--- server.js
+++ server.js
@@ -297,172 +297,12 @@ client.on('message', msg => {
 });
 
 client.on('messageReactionAdd', function(reaction, user) {
-    db.findOne({id: reaction.message.guild.id}, function(err, config) {
-        if (err) console.error(err)
-        else if (config && !reaction.message.deleted && !reaction.message.bot) {
-            var already = checkReact(reaction.message.reactions.array()) //see if bot already checked this off (e.g. already reported, passed, rejected etc)
-            
-            //MOD-VOTING CHANNEL
-            if (reaction.message.channel.name == config.channels.modvoting && reaction.message.embeds.length >= 1 && !already) {
-                
-                //upvote
-                if (reaction._emoji.name == config.upvote) {
-                    
-                    var activity_log = getChannel(reaction.message.guild.channels,config.channels.modactivity);
-                    if (activity_log) {
-                        activity_log.send(user.toString() + " just endorsed *" + reaction.message.embeds[0].footer.text + "*")
-                    }
-                    
-                    if (reaction.count == config.mod.upvoteThresh) { //all thresh comparisons changed to == to prevent double posting
-                        console.log("Proposal '"+reaction.message.embeds[0].description+"' passed")
-                        console.log("Proposal passed")
-                        reaction.message.react('✅');
-                        var ch = getChannel(reaction.message.guild.channels,config.channels.modannounce);
-                        if (ch !== null) {
-                            var old = reaction.message.embeds[0];
-                            var embed = new Discord.RichEmbed()
-                            
-                            embed.setTitle("✅ **PASSED** ✅")
-                            embed.setAuthor(old.author.name, old.author.iconURL)
-                            embed.setDescription(old.description)
-                            embed.setFooter(old.footer.text)
-                            embed.setTimestamp(new Date(old.timestamp).toString())
-                            ch.send({embed})
-                        }
-                        else {
-                            reaction.message.reply(
-                                "**The modannounce channel could not be found. Follow this syntax:**"
-                                +"```@Ohtred config modannounce channel_name```"
-                            )
-                        }
-                    }
-                }
-                
-                //downvote
-                else if (reaction._emoji.name == config.downvote) {
-                    
-                    
-                    var activity_log = getChannel(reaction.message.guild.channels,config.channels.modactivity);
-                    if (activity_log) {
-                        activity_log.send(user.toString() + " just opposed *" + reaction.message.embeds[0].footer.text + "*")
-                    }
-                    
-                    if (reaction.count == config.mod.downvoteThresh) {
-                        console.log("Proposal '"+reaction.message.embeds[0].description+"' was rejected")
-                        reaction.message.react('❌');
-                        var ch = getChannel(reaction.message.guild.channels,config.channels.modannounce);
-                        if (ch !== null) {
-                            var old = reaction.message.embeds[0];
-                            var embed = new Discord.RichEmbed()
-                            
-                            embed.setTitle("❌ **FAILED** ❌")
-                            embed.setAuthor(old.author.name, old.author.iconURL)
-                            embed.setDescription(old.description)
-                            embed.setFooter(old.footer.text)
-                            embed.setTimestamp(new Date(old.timestamp).toString())
-                            ch.send({embed})
-                        }
-                        else {
-                            reaction.message.reply(
-                                "**The modannounce channel could not be found. Follow this syntax:**"
-                                +"```@Ohtred config modannounce channel_name```"
-                            )
-                        }
-                    }
-                }
-            }
-            
-            //FEEDBACK CHANNEL
-            else if (reaction.message.channel.name == config.channels.feedback && !already) {
-                var content = reaction.message.content;
-                
-                if (reaction._emoji.name == config.upvote && reaction.count == config.pleb.upvoteThresh) {
-                    var upvotes = reaction.count;
-                    console.log("Petition passed: "+content);
-                    var ch = getChannel(reaction.message.guild.channels, config.channels.modvoting);
-                    reaction.message.react('✅');
-                    if (ch !== null) {
-                        var prop_id = Math.random().toString(36).substring(5);
-                        const embed = new Discord.RichEmbed()
-                        
-                        embed.setTitle(".:: 𝐏𝐄𝐓𝐈𝐓𝐈𝐎𝐍")
-                        embed.setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
-                        
-                        if (reaction.message.attachments.size > 0) {
-                            console.log("Image attached")
-                            embed.setDescription(content + "\n" + reaction.message.attachments.array()[0].url)
-                        }
-                        else {
-                            console.log("No image attached")
-                            embed.setDescription(content)
-                        }
-                        
-                        embed.setFooter(prop_id)
-                        embed.setTimestamp()
-                        ch.send({embed})
-                    }
-                    else {
-                        reaction.message.reply(
-                            "The modvoting channel could not be found. Follow this syntax:"
-                            +"```@Ohtred config modvoting channel_name```"
-                        )
-                    }
-                }
-                
-            }
-            
-            //REPORTABLE CHANNELS
-            else if (!already && config.reportable.indexOf(reaction.message.channel.name) != -1) { 
-                var content = reaction.message.content;
-                if (reaction._emoji.name == config.report && reaction.count >= config.pleb.reportThresh) {
-                    
-                    reaction.message.react('❌');
-                    var report_channel = getChannel(reaction.message.guild.channels, config.channels.reportlog)
-                    if (report_channel) { //if report channel exists
-                        
-                        const embed = new Discord.RichEmbed()
-                        embed.setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
-                        embed.setDescription(content)
-                        //embed.setFooter(reaction.message.url);
-                        embed.setTimestamp()
-                        
-                        if (reaction.message.attachments.size > 0) {
-                            console.log("Image attached")
-                            embed.setDescription(content + "\n" + reaction.message.attachments.array()[0].url)
-                        }
-                        else {
-                            console.log("No image attached")
-                            embed.setDescription(content)
-                        }
-                        reaction.fetchUsers().then(function(val) {
-                            var users = val.array()
-                            var replist = "**Reporters: **"
-                            for (var i = 0; i < users.length; i++) {
-                                console.log(users[i].id)
-                                replist += "<@" + users[i].id + ">" + " "
-                            }
-                            
-                            report_channel.send({embed}).then(function() { 
-                                report_channel.send(replist)
-                                report_channel.send("@here " + reaction.message.url)
-                                
-                                if (!reaction.message.member.mute) { //if he's already muted don't remute... keep timer integrity
-                                    reaction.message.member.setMute(true, "Automatically muted for 5 reports")
-                                        setTimeout(function() {
-                                            console.log(reaction.message.member.nickname + " was unmuted after 60 seconds")
-                                            reaction.message.member.setMute(false)
-                                        }, 60 * 1000) //30 second mute
-                                }
-                                
-                                reaction.message.channel.send(reaction.message.author.toString() + " just got kekked for posting illegal message")
-                                //reaction.message.delete().then(msg=>console.log("Succesfully deleted")).catch(console.error);
-                            })
-                        })
-                    }
-                }
-            }
-        }
-    })
+    if (!reaction.message.deleted && !reaction.message.bot) {
+        db.findOne({id: reaction.message.guild.id}, function(err, config) {
+            if (err) console.error(err)
+            else if (config) helper.parseReaction(reaction, user, config)
+        })
+    }
 })
 
 client.on('messageReactionRemove', function(reaction, user) {
@@ -579,6 +419,180 @@ var Helper = function() {
                 default:
                     ch.send("Bruh moment")
             }
+        }
+    }
+    
+    
+    self.parseReaction = function(reaction, user, config) {
+        if (!reaction.message.deleted && !reaction.message.bot) {
+            var already = checkReact(reaction.message.reactions.array()) //see if bot already checked this off (e.g. already reported, passed, rejected etc)
+            
+            //MOD-VOTING CHANNEL
+            if (!already && reaction.message.channel.name == config.channels.modvoting && reaction.message.embeds.length >= 1) {
+                
+                //upvote
+                if (reaction._emoji.name == config.upvote) {
+                    if (reaction.count >= config.mod.upvoteThresh) {
+                        self.react.upvote(reaction, user, config)
+                    }
+                    var activity_log = getChannel(reaction.message.guild.channels,config.channels.modactivity);
+                    if (activity_log) {
+                        activity_log.send(user.toString() + " just endorsed *" + reaction.message.embeds[0].footer.text + "*")
+                    }
+                }
+                
+                //downvote
+                else if (reaction._emoji.name == config.downvote) {
+                    if (reaction.count >= config.mod.downvoteThresh) {
+                        self.react.downvote(reaction, user, config)
+                    }
+                    var activity_log = getChannel(reaction.message.guild.channels,config.channels.modactivity);
+                    if (activity_log) {
+                        activity_log.send(user.toString() + " just opposed *" + reaction.message.embeds[0].footer.text + "*")
+                    }
+                }
+            }
+            
+            //FEEDBACK CHANNEL
+            else if (!already && reaction._emoji.name == config.upvote && reaction.message.channel.name == config.channels.feedback && reaction.count >= config.pleb.upvoteThresh) {
+                self.react.plebvote(reaction, user, config)
+            }
+        }
+        
+        //REPORTABLE CHANNELS
+        else if (!already && config.reportable.indexOf(reaction.message.channel.name) != -1) { 
+            if (reaction._emoji.name == config.report && reaction.count >= config.pleb.reportThresh) {
+                self.react.report(reaction, user, config)
+            }
+        }
+    }
+    
+    self.react = {}
+    
+    self.react.upvote = function(reaction, user, config) {
+        console.log("Proposal '"+reaction.message.embeds[0].description+"' passed")
+        console.log("Proposal passed")
+        reaction.message.react('✅');
+        var ch = getChannel(reaction.message.guild.channels,config.channels.modannounce);
+        if (ch !== null) {
+            var old = reaction.message.embeds[0];
+            var embed = new Discord.RichEmbed()
+            
+            embed.setTitle("✅ **PASSED** ✅")
+            embed.setAuthor(old.author.name, old.author.iconURL)
+            embed.setDescription(old.description)
+            embed.setFooter(old.footer.text)
+            embed.setTimestamp(new Date(old.timestamp).toString())
+            ch.send({embed})
+        }
+        else {
+            reaction.message.reply(
+                "**The modannounce channel could not be found. Follow this syntax:**"
+                +"```@Ohtred config modannounce channel_name```"
+            )
+        }
+    }
+    
+    self.react.downvote = function(reaction, user, config) {
+        console.log("Proposal '"+reaction.message.embeds[0].description+"' was rejected")
+        reaction.message.react('❌');
+        var ch = getChannel(reaction.message.guild.channels,config.channels.modannounce);
+        if (ch !== null) {
+            var old = reaction.message.embeds[0];
+            var embed = new Discord.RichEmbed()
+            
+            embed.setTitle("❌ **FAILED** ❌")
+            embed.setAuthor(old.author.name, old.author.iconURL)
+            embed.setDescription(old.description)
+            embed.setFooter(old.footer.text)
+            embed.setTimestamp(new Date(old.timestamp).toString())
+            ch.send({embed})
+        }
+        else {
+            reaction.message.reply(
+                "**The modannounce channel could not be found. Follow this syntax:**"
+                +"```@Ohtred config modannounce channel_name```"
+            )
+        }
+    }
+    
+    self.react.report = function(reaction, user, config) {
+        var content = reaction.message.content;
+        reaction.message.react('❌');
+        var report_channel = getChannel(reaction.message.guild.channels, config.channels.reportlog)
+        if (report_channel) { //if report channel exists
+            
+            const embed = new Discord.RichEmbed()
+            embed.setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
+            embed.setDescription(content)
+            //embed.setFooter(reaction.message.url);
+            embed.setTimestamp()
+            
+            if (reaction.message.attachments.size > 0) {
+                console.log("Image attached")
+                embed.setDescription(content + "\n" + reaction.message.attachments.array()[0].url)
+            }
+            else {
+                console.log("No image attached")
+                embed.setDescription(content)
+            }
+            reaction.fetchUsers().then(function(val) {
+                var users = val.array()
+                var replist = "**Reporters: **"
+                for (var i = 0; i < users.length; i++) {
+                    console.log(users[i].id)
+                    replist += "<@" + users[i].id + ">" + " "
+                }
+                
+                report_channel.send({embed}).then(function() { 
+                    report_channel.send(replist)
+                    report_channel.send("@here " + reaction.message.url)
+                    
+                    if (!reaction.message.member.mute) { //if he's already muted don't remute... keep timer integrity
+                        reaction.message.member.setMute(true, "Automatically muted for 5 reports")
+                            setTimeout(function() {
+                                console.log(reaction.message.member.nickname + " was unmuted after 60 seconds")
+                                reaction.message.member.setMute(false)
+                            }, 60 * 1000) //30 second mute
+                    }
+                    
+                    reaction.message.channel.send(reaction.message.author.toString() + " just got kekked for posting illegal message")
+                    //reaction.message.delete().then(msg=>console.log("Succesfully deleted")).catch(console.error);
+                })
+            })
+        }
+    }
+    self.react.plebvote = function(reaction, user, config) {
+        var content = reaction.message.content;
+        var upvotes = reaction.count;
+        console.log("Petition passed: "+content);
+        var ch = getChannel(reaction.message.guild.channels, config.channels.modvoting);
+        reaction.message.react('✅');
+        if (ch !== null) {
+            var prop_id = Math.random().toString(36).substring(5);
+            const embed = new Discord.RichEmbed()
+            
+            embed.setTitle(".:: 𝐏𝐄𝐓𝐈𝐓𝐈𝐎𝐍")
+            embed.setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
+            
+            if (reaction.message.attachments.size > 0) {
+                console.log("Image attached")
+                embed.setDescription(content + "\n" + reaction.message.attachments.array()[0].url)
+            }
+            else {
+                console.log("No image attached")
+                embed.setDescription(content)
+            }
+            
+            embed.setFooter(prop_id)
+            embed.setTimestamp()
+            ch.send({embed})
+        }
+        else {
+            reaction.message.reply(
+                "The modvoting channel could not be found. Follow this syntax:"
+                +"```@Ohtred config modvoting channel_name```"
+            )
         }
     }
 }
