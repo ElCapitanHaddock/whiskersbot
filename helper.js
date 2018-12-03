9304802b03bca930b6bc72941188f6429e04f8e6
ElCapitanHaddock <jkid@littlefish.us>
2018-12-03 02:13:51 +0000

    added cleancontent

--- helper.js
+++ helper.js
@@ -465,15 +465,15 @@ var Helper = function(db, Discord, perspective) {
         if (req_attr.length > 0) {
             (async function() {
                 try {
-                    const result = await perspective.analyze(msg.content, {attributes: req_attr});
+                    const result = await perspective.analyze(msg.cleanContent, {attributes: req_attr});
                     var hit = false
                     
-                    var desc = msg.author.toString() + " in " + msg.channel.toString() +  "```" + msg.content + "```" 
+                    var desc = msg.author.toString() + " in " + msg.channel.toString() +  "```" + msg.cleanContent + "```" 
                     
                     for (var i = 0; i < req_attr.length; i++) {
                         var score = Math.round(result.attributeScores[req_attr[i]].summaryScore.value * 100)
                         if (score >= 96) hit = true  
-                        desc += req_attr[i] + "...**" + score + "**\n"
+                        desc += req_attr[i] + "...**" + score + "**\n...\n"
                     }
                     
                     const embed = new Discord.RichEmbed()
