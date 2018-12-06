065ae6a01a688067b404be8301088755782ced1e
ElCapitanHaddock <jkid@littlefish.us>
2018-12-06 03:22:42 +0000

    testing user fetch

--- util.js
+++ util.js
@@ -1,16 +1,19 @@
 
 var Util = {
     //see if message is already checked off by seeing if any reactions belong to the bot itself
-    checkReact: function(reactions) {
+    checkReact: function(reactions, callback) {
         var already = false;
         for (var i = 0; i < reactions.length; i++) {
-            var users = reactions[i].fetchUsers({limit:50})
-            var users = reactions[i].users.array()
-            for (var x = 0; x < users.length; x++) {
-                if (users[x].bot == true) {
-                    already = true;
+            reactions[i].fetchUsers({limit:50}).then(function(umap) {
+                var users = umap.array()
+                var already = false
+                for (var x = 0; x < users.length; x++) {
+                    if (users[x].bot == true) {
+                        already = true
+                    }
                 }
-            }
+                callback(already)
+            })
         }
         return already
     },
