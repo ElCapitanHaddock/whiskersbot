9840c8e6ede217fe892074bcd4bd2f37ba5414ee
ElCapitanHaddock <jkid@littlefish.us>
2018-12-12 21:53:30 +0000

    fixed case sensitivity in role command

--- helpers/manage.js
+++ helpers/manage.js
@@ -124,7 +124,7 @@ var Manage = function(db, client, Discord) {
             var ro = params.slice(1).join(" ");
             
             var mem = msg.guild.members.find(m => m.id == me);
-            var diff_role = msg.guild.roles.find( r => r.name.startsWith(ro) || r.id == ro.replace(/\D/g,'') )
+            var diff_role = msg.guild.roles.find( r => r.name.toLowerCase().startsWith(ro.toLowerCase()) || r.id == ro.replace(/\D/g,'') )
             if (mem && diff_role) {
                 var check_role = mem.roles.find(r => r.id == diff_role.id) //check if user has it
                 if (!check_role && !mem.permissions.has('ADMINISTRATOR')) {
