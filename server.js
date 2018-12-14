0b51f3924bfbf2a7ef30c8ed78a94d0d555e48dd
ElCapitanHaddock <jkid@littlefish.us>
2018-12-14 17:58:27 +0000

    testing sharding

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ function init(db) {
       autoSpawn: true,
       shardArgs: [db]
     });
+    Manager.spawn(1)
 
     Manager.on('launch', shard => {
       console.log(`Launching Shard ${shard.id} [ ${shard.id + 1} of ${Manager.totalShards} ]`);
