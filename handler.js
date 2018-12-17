8fb9d1935a4175765b722f29ff3bc1572909ccd7
ElCapitanHaddock <jkid@littlefish.us>
2018-12-17 18:15:27 +0000

    topic fix

--- handler.js
+++ handler.js
@@ -85,7 +85,7 @@ var Handler = function(API, Discord,client,intercom,helper,perspective) {
             }
             else self.parseMessage(msg, cmd, ctx, true, config)
         }
-        else if (!msg.author.bot && msg.channel.topic.startsWith("#") && config.embassy && config.embassy[msg.channel.id]) {
+        else if (!msg.author.bot && config.embassy && config.embassy[msg.channel.id]) {
             API.get(msg.guild.id, function(err, other) {
                 if (err) {
                     if (err) console.error(err)
