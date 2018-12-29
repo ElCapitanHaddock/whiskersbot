cbcdf2a49c2cf5edeb7ae1a8298e9ab1cf826c33
ElCapitanHaddock <jkid@littlefish.us>
2018-12-29 08:14:34 +0000

    hotfix

--- handler.js
+++ handler.js
@@ -82,7 +82,7 @@ var Handler = function(API, Discord,client,intercom,helper,perspective) {
                         mem.removeRole(config.autorole, "Alt authentication verified").catch(console.error)
                         msg.reply("<:green_check:520403429479153674> You're in. Never share your token.").catch(console.error)
                         
-                        var report_channel = util.getChannel(msg.guild.channels, config.channels.reportlog)
+                        var report_channel = util.getChannel(gd.channels, config.channels.reportlog)
                         if (!report_channel) return
                         var embed = new Discord.RichEmbed()
                         embed.setAuthor(msg.author.tag, msg.author.displayAvatarURL)
