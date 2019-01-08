c8fefa262efc18568ae172eb9242f6306de72e8f
ElCapitanHaddock <jkid@littlefish.us>
2019-01-08 18:22:06 +0000

    testing avatar command

--- helpers/cosmetic.js
+++ helpers/cosmetic.js
@@ -1086,7 +1086,7 @@ var Cosmetic = function(perspective, translate, client, cloudinary) {
     }
     
     self.userinfo = (msg, ctx, config, cb) => {
-        if (!ctx || !ctx.trim()) ctx == msg.author.toString()
+        if (!ctx || !ctx.trim()) ctx == msg.member.toString()
         var members = msg.guild.members
         var m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.startsWith(ctx))
         if (!m) m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.toLowerCase().startsWith(ctx.toLowerCase()))
@@ -1144,6 +1144,54 @@ var Cosmetic = function(perspective, translate, client, cloudinary) {
             msg.channel.send(embed)
         }
         else cb("<:red_x:520403429835800576> Couldn't find that role!")
+    }
+    
+    self.userinfo = (msg, ctx, config, cb) => {
+        if (!ctx || !ctx.trim()) ctx == msg.member.toString()
+        var members = msg.guild.members
+        var m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.startsWith(ctx))
+        if (!m) m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.toLowerCase().startsWith(ctx.toLowerCase()))
+        if (!m) m = members.find(m => m.toString() === ctx || m.id === ctx || (m.nickname && m.nickname.toLowerCase().startsWith(m.nickname.toLowerCase())) )
+        if (m) {
+            var embed = new Discord.RichEmbed()
+            embed.setDescription(m.toString())
+            embed.setAuthor(m.user.tag, m.user.avatarURL)
+            embed.setThumbnail(m.user.avatarURL)
+            embed.setColor(m.displayColor)
+            embed.setTimestamp()
+            var options = {
+                day: 'numeric',
+                month: 'long', 
+                year: 'numeric'
+            };
+            embed.addField("Joined", m.joinedAt.toLocaleDateString("en-US", options))
+            embed.addField("Created", m.user.createdAt.toLocaleDateString("en-US", options))
+            var roles = m.roles.array()
+            var role_list = ""
+            for (var i = 0; i < roles.length; i++) {
+                role_list += roles[i].toString() + " "
+            }
+            embed.addField("Roles", role_list ? role_list : "None")
+            embed.setFooter("ID: " + m.id)
+            msg.channel.send(embed)
+        }
+        else cb("<:red_x:520403429835800576> Couldn't find that user!")
+    }
+    
+    self.avatar = (msg, ctx, config, cb) => {
+        if (!ctx || !ctx.trim()) ctx == msg.member.toString()
+        var members = msg.guild.members
+        var m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.startsWith(ctx))
+        if (!m) m = members.find(m => m.toString() === ctx || m.id === ctx || m.user.tag.toLowerCase().startsWith(ctx.toLowerCase()))
+        if (!m) m = members.find(m => m.toString() === ctx || m.id === ctx || (m.nickname && m.nickname.toLowerCase().startsWith(m.nickname.toLowerCase())) )
+        if (m) {
+            var embed = new Discord.RichEmbed()
+            embed.setAuthor(m.user.tag, m.user.avatarURL)
+            embed.setImage(m.user.avatarURL)
+            embed.setURL(m.user.avatarURL)
+            msg.channel.send(embed).catch(console.error)
+        }
+        else cb("<:red_x:520403429835800576> Couldn't find that user!")
     }
 }
 module.exports = Cosmetic
