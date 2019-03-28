a953ff7d19f36af8e5ce8c5cc6650b3de59f8a04
ElCapitanHaddock <jkid@littlefish.us>
2019-03-28 17:09:31 +0000

    fixed puppeteer for heroku

--- helpers/cosmetic.js
+++ helpers/cosmetic.js
@@ -150,7 +150,10 @@ var Cosmetic = function(perspective, translate, client, cloudinary) {
         
         var random = Math.random().toString(36).substring(4);
         (async () => {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({'args' : [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]});
             const page = await browser.newPage();
             await page.goto(ctx).catch(
                 cb("404: URL not found!")
