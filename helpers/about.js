8b3f1a4e69fd2df149c3b8ef568679e0c96feaf3
ElCapitanHaddock <jkid@littlefish.us>
2019-04-12 03:46:33 +0000

    test

--- helpers/about.js
+++ helpers/about.js
@@ -4,19 +4,25 @@ var About = function(client) {
     this.setup = (msg, config, cb) => {
         var embed = new Discord.RichEmbed()
         embed.setTitle("Setting up whiskers")
-        embed.addField("prefix [prefix]", "to set the server prefix")
-        embed.addField("channel [channel_type] [channel_mention]", "to link/unlink one of the features to a channel.\nTypes: **modvoting,modannounce,modactivity,feedback,reportlog,verifylog**")
-        embed.addField("emote [upvote|downvote|report] [emote]", "to set the emote to its corresponding mechanic.")
-        embed.addField("permit [role]", "to permit a rolename to interact with me. If the role is unmentionable, use its ID instead")
-        embed.addField("unpermit [role]", "to remove a role from interacting with me")
-        embed.addField("reportable [channel]", "to add/remove a channel to the list where messages are reportable")
-        embed.addField("blacklist [channel]", "to blacklist a channel")
-        embed.addField("unblacklist [channel]", "to unblacklist a channel")
-        embed.addField("config [mod_upvote|mod_downvote|mod_upvote2|mod_downvote2|petition_upvote|report_vote] [count]", "to set a voting threshold")
-        embed.addField("report_time [time]", "to set the amount of time a user gets muted for a report")
-        embed.addField("counter [number 1-50]", "to set the change in # of users online in order to update the counter.\nIncrease if it's flooding your audits, decrease if it's not updating fast enough.")
-        embed.addField("about usage", "learn how to use whiskers after you set everything up\n......\n")
-        embed.addField("**Join the support server if you need help**", "https://discord.gg/46KN5s8")
+        embed.addField("`prefix [prefix]`", "to set the server prefix\n\u200b")
+        embed.addField("`channel [channel_type] [channel_mention]`", "to link/unlink one of the features to a channel.\n```Types: modvoting,modannounce,modactivity,feedback,reportlog,verifylog```")
+        embed.addField("`emote [upvote|downvote|report] [emote]`", "to set the emote to its corresponding mechanic.\n\u200b")
+        embed.addField("`permit [role]`", "to permit a rolename to interact with me. If the role is unmentionable, use its ID instead")
+        embed.addField("`unpermit [role]`", "to remove a role from interacting with me\n\u200b")
+        embed.addField("`reportable [channel]`", "to add/remove a channel to the list where messages are reportable")
+        embed.addField("`blacklist [channel]`", "to blacklist a channel")
+        embed.addField("`unblacklist [channel]`", "to unblacklist a channel\n\u200b")
+        embed.addField("`config [mod_upvote|mod_downvote|mod_upvote2|mod_downvote2|petition_upvote|report_vote] [count]`", "to set a voting threshold")
+        embed.addField("`report_time [time]`", "to set the amount of time a user gets muted for a report")
+        embed.addField("`counter [number 1-50]`", "to set the change in # of users online in order to update the counter.\nIncrease if it's flooding your audits, decrease if it's not updating fast enough.\n\u200b")
+        
+        embed.addField("`about voting`", "learn how to use whiskers for voting")
+        embed.addField("`about verification`", "learn how to setup whiskers verification")
+        embed.addField("`about embassy`", "learn how to setup whiskers embassies")
+        embed.addField("`about management`", "learn how to use whiskers to moderate")
+        embed.addField("`about usage`", "learn how to general whiskers commands")
+        embed.addField("`about server`", "to see what the current whiskers settings are\n\u200b")
+        embed.addField("`**Join the support server if you need help**", "https://discord.gg/HnGmt3T")
         cb(null, embed)
     }
     
@@ -24,30 +30,30 @@ var About = function(client) {
         var embed = new Discord.RichEmbed()
         embed.setTitle("The default prefix is @whiskers")
         
-        embed.addField("...\nVoting commands", "...")
-        embed.addField("propose [description]", "to put your idea to vote", true)
-        embed.addField("motion [threshold] [description]", "for a custom admin vote",true)
-        embed.addField("alert [severity 1-4]", "to notify the mods to an emergency")
+        embed.addField("`propose [description]`", "to put your idea to vote", true)
+        embed.addField("`motion [threshold] [description]`", "for a custom admin vote",true)
+        embed.addField("`alert [severity 1-4]`", "to notify the mods to an emergency\n\u200b")
         
-        embed.addField("...\nText commands", "...")
-        embed.addField("query [text]", "to get Google search term popularity")
-        embed.addField("wikipedia [title]", "to get Wikipedia summary")
-        embed.addField("yahoo [question]", "to scrape YahooAnswers")
-        embed.addField("kym [meme name]", "to scrape KnowYourMeme")
-        embed.addField("scp [0000]", "to scrape for an SCP")
-        embed.addField("lookup [text]", "to find info about a person/place/thing (1.5 billion data entries)")
-        embed.addField("analyze [type] [text]", "to predict toxicity of a text")
-        embed.addField("translate [language] [text]", "to translate to that language", true)
+        embed.addField("`query [text]`", "to get Google search term popularity + graph")
+        embed.addField("`geo [region] [text]`", "to get a Google search-term map")
+        embed.addField("`wikipedia [title]`", "to get Wikipedia summary")
+        embed.addField("`yahoo [question]`", "to scrape YahooAnswers")
+        embed.addField("`kym [meme name]`", "to scrape KnowYourMeme")
+        embed.addField("`scp [0000]`", "to scrape for an SCP")
+        embed.addField("`redditor [username]`", "to scrape for a redditor")
+        embed.addField("`scan [http url]`", "to scan a website and produce a screenshot, good for checking NSFW")
+        embed.addField("`lookup [text]`", "to find info about a person/place/thing (1.5 billion data entries)")
+        embed.addField("`analyze [type] [text]`", "to predict toxicity of a text")
+        embed.addField("`translate [language] [text]`", "to translate to that language\n\u200b")
         
-        embed.addField("...\nImage commands", "...")
-        embed.addField("classify [image url]", "to analyze and caption an image")
-        embed.addField("identify [image url]", "to guess what an image is", true)
-        embed.addField("landmark [image url]", "to put an image (irl) on the map")
-        embed.addField("locate [image url]", "to find wherever the image is found online")
-        embed.addField("read [image url]", "to grab text from an image",true)
-        embed.addField("mirror/similar [image url]", "to find an identical/similar image online")
-        embed.addField("meme [url] [cap|tion]", "to make a meme")
-        embed.addField("\n...**OTHER**", "Report messages with your server's :report: emote\n"
+        embed.addField("`classify [image url]`", "to analyze and caption an image")
+        embed.addField("`identify [image url]`", "to guess what an image is", true)
+        embed.addField("`landmark [image url]`", "to put an image (irl) on the map")
+        embed.addField("`locate [image url]`", "to find wherever the image is found online")
+        embed.addField("`read [image url]`", "to grab text from an image",true)
+        embed.addField("`mirror/similar [image url]`", "to find an identical/similar image online")
+        embed.addField("`meme [url] [cap|tion]`", "to make a meme\n\u200b")
+        embed.addField("**OTHER**","Report messages with your server's :report: emote\n"
         + "Name a category 🔺 and it will turn it into an online users counter",true)
         cb(null, embed)
     }
@@ -58,11 +64,11 @@ var About = function(client) {
         embed.setDescription(
             "For there to be any verification at all, autorole must be set. For alt verification to be logged, the verifylog must be set. In contrast, lockdown does not require an autorole")
 
-        embed.addField("lockdown [number 0-2]", "to lockdown the server against raiders [0: none, 1: autokick (only kicks once!), 2: autoban (permanent)]")
-        embed.addField("autorole [role]", "to set a **verification** autorole")
-        embed.addField("password [reset|set|get]", "resets, sets, or gets the password. Reset it to disable the bypass feature. Set it to enable password verification to remove autorole upon join. For it to work, autorole must be enabled as well.")
-        embed.addField("verify_age [time, e.g. 5 days]", "to only autorole accounts younger than the set age.")
-        embed.addField("verification [0-4]", "to set anti-alt verification. More about the levels below.\n\u200b")
+        embed.addField("`lockdown [number 0-2]`", "to lockdown the server against raiders [0: none, 1: autokick (only kicks once!), 2: autoban (permanent)]")
+        embed.addField("`autorole [role]`", "to set a **verification** autorole")
+        embed.addField("`password [reset|set|get]`", "resets, sets, or gets the password. Reset it to disable the bypass feature. Set it to enable password verification to remove autorole upon join. For it to work, autorole must be enabled as well.")
+        embed.addField("`verify_age [time, e.g. 5 days]`", "to only autorole accounts younger than the set age.")
+        embed.addField("`verification [0-4]`", "to set anti-alt verification. More about the levels below.\n\u200b")
         
         embed.addField("Level 0", "all new joiners will be added to the autorole (if set) and must be manually verified.")
     	embed.addField("Level 1,2,3,4", "all new joiners will need to visit an external verification page to be allowed in, which requires that they have specified # of **connected account.**")
@@ -74,17 +80,16 @@ var About = function(client) {
     this.management = (msg, config, cb) => {
         var embed = new Discord.RichEmbed()
         embed.setTitle("Management Commands")
-        embed.addField("mute [user] [time]", "to mute a user", true)
-        embed.addField("unmute [user]", "to unmute a user",true)
-        embed.addField("kick [user]", "to kick a user (requires admin)")
-        embed.addField("ban [user]", "to ban a user (requires admin)",true)
-        embed.addField("unban [user]", "to unban a user (requires admin)", true)
-        embed.addField("role [user] [role]", "to add/remove a role from a user", true)
-        embed.addField("warn [user] [text]", "to send a user a warning DM", true)
-        embed.addField("wash/purge [1-100]", "to purge messages from the channel", true)
-        embed.addField("autorole [role]", "to set the verification autorole", true)
-        embed.addField("mutedrole [role]", "to set a muted role (for use with the mute command)")
-        embed.addField("Automod","@whiskers about automod")
+        embed.addField("`mute [user] [time]`", "to mute a user", true)
+        embed.addField("`unmute [user]`", "to unmute a user",true)
+        embed.addField("`kick [user]`", "to kick a user (requires admin)")
+        embed.addField("`ban [user]`", "to ban a user (requires admin)",true)
+        embed.addField("`unban [user]`", "to unban a user (requires admin)", true)
+        embed.addField("`role [user] [role]`", "to add/remove a role from a user", true)
+        embed.addField("`warn [user] [text]`", "to send a user a warning DM", true)
+        embed.addField("`wash/purge [1-100]`", "to purge messages from the channel", true)
+        embed.addField("`autorole [role]`", "to set the verification autorole", true)
+        embed.addField("`mutedrole [role]`", "to set a muted role (for use with the mute command)")
         cb(null, embed)
     }
     
@@ -225,8 +230,7 @@ var About = function(client) {
     this.credits = (msg, config, cb) => {
         cb(null, "```This bot was envisioned and entirely programmed by me, but I couldn't have done it entirely myself.\n"
         + "Thanks to the meticulous testing and input of the people of /r/okbuddyretard and /r/bruhmoment.\n"
-        + "Thanks to Yandex and PerspectiveAPI for their generously APIs.\n"
-        + "Thanks to LunarShadows for helping with the PFP and setting up the support server!\n...\n"
+        + "Thanks to Yandex, Google Cloud, and PerspectiveAPI for their generously APIs.\n...\n"
         + "And most of all, thanks to YOU, for choosing my bot. I hope it works out for you.```\nIf you're feeling generous, please give my bot an upvote: https://discordbots.org/bot/528809041032511498")
     }
     
