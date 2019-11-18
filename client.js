4d54bd63d362c2392c9feef43dec739f3ddfdd2c
ElCapitanHaddock <jkid@littlefish.us>
2019-11-18 23:38:17 +0000

    changed interval to 3min

--- client.js
+++ client.js
@@ -140,7 +140,7 @@ function checkMutes() {
     })
 }
 
-const muteCheckInterval = 2 //in minutes
+const muteCheckInterval = 3 //in minutes
 setInterval(checkMutes, muteCheckInterval * 60 * 1000)
 
 
