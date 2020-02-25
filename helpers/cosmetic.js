1e8fa65c29a12da3d571e3c4352ba3af64e872c0
ElCapitanHaddock <jkid@littlefish.us>
2020-02-25 01:54:02 +0000

    readded pokemon command which mysteriously dissappeared

--- helpers/cosmetic.js
+++ helpers/cosmetic.js
@@ -399,6 +399,30 @@ var Cosmetic = function(API, perspective, translate, client, cloudinary, dbl) {
           })
     }
     
+    self.poke = (msg, ctx, config, cb) => {
+        var params = ctx.split(" ")
+        var a = isNaN(parseInt(params[0])) ? Math.floor(Math.random() * 152) + 1 : params[0];
+        var b = isNaN(parseInt(params[1])) ? Math.floor(Math.random() * 152) + 1 : params[1];
+        var url = "http://pokemon.alexonsager.net/"+a+"/"+b
+        scrapeIt(url, {
+          img:{
+              selector:"#pk_img",
+              attr:"src"
+          },
+          name:"#pk_name"
+        })
+        .then(({ data, response }) => {
+        	var embed = new Discord.RichEmbed()
+            embed.setTitle(data.name)
+            embed.setURL(url)
+            embed.setImage(data.img)
+            embed.setFooter(a + "/" + b, "https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG")
+            
+            msg.channel.send(embed).catch(console.error)
+        })
+        .catch(console.error)
+    }
+    
     self.wutang = (msg, ctx, config, cb) => {
         if (ctx.trim().length == 0) {
             ctx = msg.author.username
